ecd4e2db0063934658e8afb9171dff2e6472ee7e
Yaser2206 <yaserarafath2210@gmail.com>
2023-11-06 00:59:06 +0530

    Flask

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
-
-app = Flask(__name__)
+from flask import Flask,request, render_template
+app=Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return render_template('index.html')
+@app.route("/flask2")
+def show_firstPage():
+    return render_template("index2.html")
+
+@app.route("/nextPage",methods=['GET', 'Post'])
+def next_page():
+    return render_template("nextPage.html")
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
